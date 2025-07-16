27228b39e8d99409b881504894c00d342a5f0d99
kopilashrestha9033 <kopila.shrestha9033@gmail.com>
2025-07-16 17:08:45 -0600

    Update ProductPage.ts

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -4,8 +4,12 @@ export class ProductPage {
   constructor(private page: Page) {}
 
   async configureAndAddToCart() {
-    await this.page.locator('.swatch-option.text').first().click();
-    await this.page.locator('.swatch-option.color').first().click();
+    await this.page.locator('.swatch-option.text').first().scrollIntoViewIfNeeded();
+    await this.page.locator('.swatch-option.text').first().click({ timeout: 5000 });
+    
+    await this.page.locator('.swatch-option.color').first().scrollIntoViewIfNeeded();
+    await this.page.locator('.swatch-option.color').first().click({ timeout: 5000 });
+    
     await this.page.click('button:has-text("Add to Cart")');
   }
 }
